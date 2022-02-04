c6b39ba705befcafabe507c491df5b68cd9f11e3
1Sherlock <=>
2022-02-04 11:48:45 +0500

    fixed

---
+++ app-tour-server/src/main/java/uz/soft/cosmos/apptourserver/AppTourServerApplication.java
@@ -0,0 +1,13 @@
+package uz.soft.cosmos.apptourserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTourServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppTourServerApplication.class, args);
+    }
+
+}
