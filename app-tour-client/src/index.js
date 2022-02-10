80e4d4410be2bbe952eb9758656466e9aef3d9fa
nizom702 <nizom702@gmail.com>
2022-02-10 16:27:04 +0500

    redux

--- app-tour-client/src/index.js
+++ app-tour-client/src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.css";
+import 'react-toastify/dist/ReactToastify.css';
 import "./styles/global.scss";
+import {applyMiddleware, createStore} from "redux";
+import {rootReducer} from "./redux/reducers/rootReducer";
+import {Provider} from "react-redux";
+import thunk from "redux-thunk";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
 );
